f972bcdc339bee5ff39efea02b7316a31c59f522
RyanJoeW <Ryanwoestenberg@hotmail.com>
2025-11-27 14:36:34 +0100

    wat dingen aangepast in mijn test om te kijken of ik nu wel kan mergen

--- DonTapDaBomb/src/test/java/com/ryan/dontapdabomb/dontapdabomb/UserTests/UserServiceTests.java
+++ DonTapDaBomb/src/test/java/com/ryan/dontapdabomb/dontapdabomb/UserTests/UserServiceTests.java
@@ -20,8 +20,8 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+//@SpringBootTest
+//@AutoConfigureMockMvc
 
 class UserServiceTest {
 
