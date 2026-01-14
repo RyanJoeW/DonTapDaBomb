33164cea94c510460b1fd82af0652fe4d10ef3ab
RyanJoeW <Ryanwoestenberg@hotmail.com>
2026-01-14 11:31:01 +0100

    test database in memorie database veranderd voor cicd tests

--- DonTapDaBomb/src/test/java/com/ryan/dontapdabomb/dontapdabomb/GameTests/GameIntergrationTests.java
+++ DonTapDaBomb/src/test/java/com/ryan/dontapdabomb/dontapdabomb/GameTests/GameIntergrationTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
